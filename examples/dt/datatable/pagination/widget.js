1b22ad145ba11d3f445f6ba2d733ba87e1e2c284
onvno <leewei2020@126.com>
2016-07-14 16:02:56 +0800

    sonar修改，build

--- examples/dt/datatable/pagination/widget.js
+++ examples/dt/datatable/pagination/widget.js
@@ -3,11 +3,11 @@ var element = document.getElementById('pagination');
 var comp = new u.pagination({el:element,jumppage:true});
 comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,});
 
-this.comp.on('pageChange', function (pageIndex) {
+comp.on('pageChange', function (pageIndex) {
     console.log('新的页号为' + pageIndex);
 });
 
-this.comp.on('sizeChange', function (arg) {
+comp.on('sizeChange', function (arg) {
     console.log('每页显示条数为' + arg[0]);
 });
 
