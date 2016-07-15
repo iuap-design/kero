5a908d9f1fe4f40355e711e176f888c36e9d7ce7
onvno <leewei2020@126.com>
2016-07-15 10:13:29 +0800

    sonar fix

--- vendor/uui/js/u.js
+++ vendor/uui/js/u.js
@@ -14698,9 +14698,11 @@ Row.fn._getSimpleData = function(data){
                     _data[key] = _dateToUTCString(data[key].value)
                 }
             }
-        }else if(typeof data[key].value !== 'undefined'){
-            _data[key] = undefined;
-        }else{
+        }
+        // else if(typeof data[key].value !== 'undefined'){
+        //     _data[key] = undefined;
+        // }
+        else{
             _data[key] = this._getSimpleData(data[key])
         }
     }
