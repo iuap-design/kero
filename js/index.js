596e8c906a83231d0b9f6ffb3a0215c6481a5454
Kvkens <kvkens@qq.com>
2016-08-08 15:32:16 +0800

    添加web pack构建工具以及相关package依赖.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Module : Kero webpack entry index
+ * Author : Kvkens(yueming@yonyou.com)
+ * Date	  : 2016-08-08 15:24:46
+ */
+
+
+import { u } from 'neoui-sparrow';
+console.log(u.addClass(document.querySelector("body"),"Wrap"));
